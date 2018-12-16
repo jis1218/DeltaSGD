98d5ea4563a42b3957113795ffb5ddad342adcd7
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-12-16 23:19:43 +0900

    comit

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,12 @@ public class Main {
 		
 		for(int m=0; m<100000; m++){
 			for(int i=0; i<sample.length; i++){
+				//DoSGDDelta 함수는 샘플 한개당 한번씩 weight를 갱신한다.
 				weight = sgdDelta.DoSGDDelta(sample[i], weight, target[i]);
 			}
 		}
 		
+		// output을 담을 배열 생성
 		double output[] = new double[sample.length];
 		
 		// output 출력하는 함수
@@ -23,23 +25,25 @@ public class Main {
 				sum += sample[k][j]*weight[j];
 			}
 			output[k] = sgdDelta.sigmoid(sum);
-			//System.out.println(output[k]);
+			System.out.println(output[k]);
 		}
 		
-		double batch_output[] = new double[sample.length];
 		
-		for(int z=0; z<100; z++){
+		double batch_output = 0;
+		
+		for(int z=0; z<100000; z++){
+			// batch 방법으로 결과 도출해보기, 샘플 세트당 한번씩 weight를 갱신한다.
 			weight = batch.doBatch(sample, weight, target);
 		}
 		
+		// output 출력하는 함수
 		for(int w=0; w<sample.length; w++){
 			double output_sum = 0;
 			for(int y=0; y<weight.length; y++){
 				output_sum += sample[w][y]*weight[y];
 			}
-			batch_output[w] = batch.sigmoid(output_sum);
-			System.out.println(batch_output[w]);
+			batch_output = batch.sigmoid(output_sum);
+			System.out.println(batch_output);
 		}
 	}
-
 }
